e7a0472705e784b1ee388a73b776d6187c5d743c
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-09-15 01:02:10 +0530

    ADDED: Using streams to find min and max of even numbers

--- src/main/java/com/bridgelabz/javastreams/JavaStreamsMain.java
+++ src/main/java/com/bridgelabz/javastreams/JavaStreamsMain.java
@@ -10,7 +10,7 @@ public class JavaStreamsMain {
 	public static void main(String[] args) {
 
 		ArrayList<Integer> myNumberList = new ArrayList<Integer>();
-		for (int i = 0; i < 5; i++)
+		for (int i = 0; i < 15; i++)
 			myNumberList.add(i);
 		myNumberList.stream().forEach(n -> System.out.println("number is: " + n));
 
@@ -29,6 +29,17 @@ public class JavaStreamsMain {
 				.findFirst()
 				.orElse(null);
 		System.out.println("The first even number is :"+firstEvenNumber);
+
+		Integer minEvenNumber=myNumberList.stream()
+				.filter(isEvenFunction)
+				.min(Integer::compare).orElse(null);
+		System.out.println("Minimum even number is: "+minEvenNumber);
+
+		Integer maxEvenNumber = myNumberList.stream()
+				.filter(isEvenFunction)
+				.max(Integer::compare)
+				.orElse(null);
+		System.out.println("Maximum even number is: "+maxEvenNumber);
 	}
 
 }
