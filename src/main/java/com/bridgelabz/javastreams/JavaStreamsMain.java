bed9f653d6bb41fc730c168bac2d4ac9357bec94
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-09-15 00:48:57 +0530

    ADDED: Code to convert numbers to double and store using streams

--- src/main/java/com/bridgelabz/javastreams/JavaStreamsMain.java
+++ src/main/java/com/bridgelabz/javastreams/JavaStreamsMain.java
@@ -1,6 +1,9 @@
 package com.bridgelabz.javastreams;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class JavaStreamsMain {
 	public static void main(String[] args) {
@@ -9,6 +12,13 @@ public class JavaStreamsMain {
 		for (int i = 0; i < 5; i++)
 			myNumberList.add(i);
 		myNumberList.stream().forEach(n -> System.out.println("number is: " + n));
+
+		Function<Integer, Double> toDoubleFunction = Integer::doubleValue;
+
+		List<Double> doubleList=myNumberList.stream()
+				.map(toDoubleFunction)
+				.collect(Collectors.toList());
+		System.out.println("Obtained double list is "+doubleList);
 	}
 
 }
