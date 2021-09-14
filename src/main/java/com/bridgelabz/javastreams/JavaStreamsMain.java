0df43c7f5aea860ca963f374cc4b96300d10560b
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-09-15 00:52:43 +0530

    ADDED: Logic to filter even numbers from stream & store it to a list

--- src/main/java/com/bridgelabz/javastreams/JavaStreamsMain.java
+++ src/main/java/com/bridgelabz/javastreams/JavaStreamsMain.java
@@ -3,6 +3,7 @@ package com.bridgelabz.javastreams;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class JavaStreamsMain {
@@ -14,11 +15,15 @@ public class JavaStreamsMain {
 		myNumberList.stream().forEach(n -> System.out.println("number is: " + n));
 
 		Function<Integer, Double> toDoubleFunction = Integer::doubleValue;
+		Predicate<Integer> isEvenFunction = n -> n > 0 && n % 2 == 0;
 
 		List<Double> doubleList=myNumberList.stream()
 				.map(toDoubleFunction)
 				.collect(Collectors.toList());
 		System.out.println("Obtained double list is "+doubleList);
+
+		List<Integer> evenStreamList=myNumberList.stream().filter(isEvenFunction).collect(Collectors.toList());
+		System.out.println("Even number list is: "+evenStreamList);
 	}
 
 }
